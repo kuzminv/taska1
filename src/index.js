dac7fb13666cca46d480796b1f5b089a61a8cd20
Viktor Kuzmin <victor.kuzmin@jetrockets.ru>
2016-11-08 18:06:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'JS World',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
