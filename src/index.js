b44293cc91ede8474518b2fdea532a3520727ff1
Viktor Kuzmin <victor.kuzmin@jetrockets.ru>
2016-11-09 20:56:10 +0300

    map problem method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,73 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+import bodyParser from 'body-parser';
+import saveDataInDb from './saveDataInDb';
+import Pet from './models/Pet';
+import User from './models/User';
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://publicdb.mgbeta.ru/kuzminv_skb3');
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  return res.json(users);
+});
+app.get('/pets', async (req, res) => {
+  const pets = await Pet.find().populate('owner');
+  return res.json(pets);
+});
+app.post('/data', async (req, res) => {
+  const data = req.body;
+  // console.log(data);
+  return res.json(await saveDataInDb(data));
+
+
+
+
+
+
+
+
+
+
+  // return res.json(data);
+
+
+
+
+  // console.log(data);
+  // return res.json({
+  //   data,
+  // });
+
+  // const data = {
+  //   user: {
+  //     name: 'kuzminv2',
+  //   },
+  //   pets: [
+  //     {
+  //       name: 'Zildjian2',
+  //       type: 'cat',
+  //     },
+  //     {
+  //       name: 'Doge2',
+  //       type: 'dog',
+  //     },
+  //   ],
+  // };
+
+
+
 });
 
-app.get('/task2A', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum.toString());
-})
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
---
+++ src/models/Pet.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import _ from 'lodash';
+const { Schema } = mongoose;
+
+const PetSchema = new Schema({
+  type: String,
+  name: String,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+}, {
+    timestamps: true,
+});
+PetSchema.methods.toJSON = function () {
+  return _.pick(this, ['name', 'type', 'owner']);
+};
+
+export default mongoose.model('Pet', PetSchema);
