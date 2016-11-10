345274cb896142e5bafb37ae25a8001d2a1a3261
Viktor Kuzmin <victor.kuzmin@jetrockets.ru>
2016-11-11 02:48:07 +0300

    add error message

--- src/canonize.js
+++ src/canonize.js
@@ -1,6 +1,5 @@
 export default function canonoze(url) {
-
-   const re = new RegExp('@?(https?:)?(\/\/)?((www.)?([а-яёa-z0-9])[^\/]*\/)?@?([a-zA-Z0-9._-]*)', 'i');
+  const re = new RegExp('@?(https?:)?(\/\/)?((www.)?([а-яёa-z0-9])[^\/]*\/)?@?([a-zA-Z0-9._-]*)', 'i');
   const username = url.match(re);
-  return '@' + username[6];
+  return username[6];
 }
--- src/index.js
+++ src/index.js
@@ -11,8 +11,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/canonize', (req, res) => {
-  const nameParse = canonize(req.query.username);
- 	res.send(nameParse);
+  const errorMessage = 'Invalid username';
+  try {
+    const nameParse = canonize(req.query.username);
+    if (!nameParse) {
+      return res.send(errorMessage);
+    }
+    res.send('@' + nameParse);
+  } catch (e) {
+    res.send(errorMessage);
+  }
 });
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
